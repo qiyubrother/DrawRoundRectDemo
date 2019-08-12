90ff6a58c88bc27a38e0dc5b2658113087725560
zhenhua Liu <qiyubrother@outlook.com>
2019-08-12 19:00:06 +0800

    添加项目文件。

---
+++ DrawRoundRectDemo/FrmMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DrawRoundRectDemo
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlMain.Controls.Add(this.btnCancel);
+            this.pnlMain.Location = new System.Drawing.Point(22, 23);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(754, 403);
+            this.pnlMain.TabIndex = 0;
+            this.pnlMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlMain_Paint);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(629, 343);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(105, 41);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "关闭";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ DrawRoundRectDemo/FrmMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawRoundRectDemo
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            ControlBox = false;
+            FormBorderStyle = FormBorderStyle.None;
+            ShowInTaskbar = false;
+            TopMost = true;
+            Text = string.Empty;
+            //Opacity = 50;
+            BackColor = Color.Black;
+            TransparencyKey = System.Drawing.Color.Black;
+            StartPosition = FormStartPosition.CenterScreen;
+            btnCancel.Click += (o, ex) => Close();
+        }
+
+        private void Draw(Rectangle rectangle, Graphics g, int _radius)
+        {
+            //g.DrawPath(new Pen(Color.Blue), DrawRoundRect(rectangle.X, rectangle.Y, rectangle.Width - 2, rectangle.Height - 1, _radius));
+            g.FillPath(new SolidBrush(Color.FromArgb(255, 245, 246, 251)), DrawRoundRect(rectangle.X, rectangle.Y, rectangle.Width - 2, rectangle.Height - 1, _radius));
+        }
+        public static GraphicsPath DrawRoundRect(int x, int y, int width, int height, int radius)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddArc(x, y, radius, radius, 180, 90);
+            gp.AddArc(width - radius, y, radius, radius, 270, 90);
+            gp.AddArc(width - radius, height - radius, radius, radius, 0, 90);
+            gp.AddArc(x, height - radius, radius, radius, 90, 90);
+            gp.CloseAllFigures();
+            return gp;
+        }
+        private void pnlMain_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;  //图片柔顺模式选择
+            Draw(e.ClipRectangle, e.Graphics, 50);
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            pnlMain.Invalidate();
+        }
+    }
+}
